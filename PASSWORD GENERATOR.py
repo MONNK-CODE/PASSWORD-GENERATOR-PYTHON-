30d9cc72cc98c28cbee47ad82ff58d3a5f4f95dc
MONNK-CODE <153551248+MONNK-CODE@users.noreply.github.com>
2024-01-14 20:26:05 -0600

    Add files via upload

---
+++ PASSWORD GENERATOR.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import random
+import string
+
+def generate_password(length):
+    lowercase_letters = string.ascii_lowercase
+    uppercase_letters = string.ascii_uppercase
+    digits = string.digits
+    special_characters = '!@#$%^&*()_+=-[]{}|;:,.<>?'
+    
+    all_characters = lowercase_letters + uppercase_letters + digits + special_characters
+
+    while True:
+        if length < 4:
+            print("Error: Password length must be at least 4 characters.")
+            return None
+
+        password = random.choice(lowercase_letters) + random.choice(uppercase_letters) + random.choice(digits) + random.choice(special_characters)
+        password += ''.join(random.choice(all_characters) for _ in range(length - 4))
+
+        password_list = list(password)
+        random.shuffle(password_list)
+        password = ''.join(password_list)
+
+        return password
+
+while True:
+    try:
+        print(" It has to be greater or equals to 4")
+        password_length = int(input("Enter the desired password length:"))
+        password = generate_password(password_length)
+        
+        if password:
+            print(f"Generated Password: {password}")
+        
+            another = input("Do you want to generate another password (yes/no): ")
+            if another == 'yes':
+                continue
+            else:
+                print("Thanks for using Muhais's Password Generator")
+                break  # Exit the loop
+                
+    except ValueError:
+        print("Error: Invalid input. Please enter a valid number for password length.")
+
+
+# In[ ]:
+
+
+
+
